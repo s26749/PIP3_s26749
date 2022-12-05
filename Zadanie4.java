d1de194e3f7472a20350ac322c52f417eb5ea3d4
s26749 <116458622+s26749@users.noreply.github.com>
2022-12-05 16:26:03 +0100

    Update Zadanie4.java

--- Zadanie4.java
+++ Zadanie4.java
@@ -1,9 +1,30 @@
 public class Zadanie4 {
     public static void main(String[] args) {
 
+
+        int x = 5;
+        int y = 3;
+        int z = 8;
+        method(x, y, z);
+
+
     }
 
-    private static void method(int x,int y,int z){
+    private static void method(int x, int y, int z) {
+        for (int i = 1; i < z; i++) {
+            if (i <= (z - x)) {
+                System.out.println();
+            } else System.out.println("* ");
+
+            if (i < (z - y)) {
+                System.out.print("");
+            } else System.out.print("* ");
+
+
+        }
+        System.out.println("* ");
 
     }
 }
+
+
